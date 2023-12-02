c664fd01b94e0ccc1fc7edd117f1a9c9894d05df
Léo GATELLIER <26511053+lgatellier@users.noreply.github.com>
2023-12-02 10:24:44 +0100

    add day02

---
+++ day02/combinations_part1.py
@@ -0,0 +1,44 @@
+CUBES = {
+    'red': 12,
+    'green': 13,
+    'blue': 14
+}
+
+LINE = "Game 79: 7 red, 1 green; 1 blue, 6 red, 2 green; 1 blue, 12 red"
+
+def read_file(input):
+    with open(input, 'r') as f:
+        return [line.strip() for line in f.readlines()]
+
+def is_game_possible(line) -> bool:
+    game_id = int(line.split(':')[0].split(' ')[1])
+    game_sets = [ single_set.strip() for single_set in line.split(':')[1].split(';') ]
+    for game_set in game_sets:
+        if is_set_possible(game_set):
+            print(f"Set {game_set} is possible")
+        else:
+            return game_id, False
+    print(f"Game {game_id} is possible")
+    return game_id, True
+
+def is_set_possible(set) -> bool:
+    set_cubes = [ single_cube.strip() for single_cube in set.split(',') ]
+    for set_cube in set_cubes:
+        cube_color = set_cube.split(' ')[1]
+        cube_quantity = int(set_cube.split(' ')[0])
+        if cube_quantity > CUBES[cube_color]:
+            return False
+    return True
+
+def main():
+    input = "input"
+    lines = read_file(input)
+    possible_games = []
+    for line in lines:
+        game_id, possible = is_game_possible(line)
+        if possible:
+            possible_games.append(game_id)
+    print(f"Possible games: {possible_games}")
+    print(sum(possible_games))
+
+main()
---
+++ day02/combinations_part2.py
@@ -0,0 +1,60 @@
+COLORS = ['red', 'green', 'blue']
+
+LINE = "Game 79: 7 red, 1 green; 1 blue, 6 red, 2 green; 1 blue, 12 red"
+
+def read_file(input):
+    with open(input, 'r') as f:
+        return [line.strip() for line in f.readlines()]
+
+def init_colors_count():
+    colors_count = {}
+    for color in COLORS:
+        colors_count[color] = 0
+    return colors_count
+
+def game_minimum_cubes(line) -> bool:
+    game_id = int(line.split(':')[0].split(' ')[1])
+    game_sets = [ single_set.strip() for single_set in line.split(':')[1].split(';') ]
+    
+    minimum_cubes = init_colors_count()
+
+    for game_set in game_sets:
+        set_minimum_cubes = game_set_minimum_cubes(game_set)
+        for cube_color in COLORS:
+            if set_minimum_cubes[cube_color] > minimum_cubes[cube_color]:
+                minimum_cubes[cube_color] = set_minimum_cubes[cube_color]
+
+    return game_id, minimum_cubes
+
+def game_set_minimum_cubes(single_set) -> tuple:
+    set_cubes = [ single_cube.strip() for single_cube in single_set.split(',') ]
+    minimum_cubes = init_colors_count()
+
+    for set_cube in set_cubes:
+        cube_color = set_cube.split(' ')[1]
+        cube_quantity = int(set_cube.split(' ')[0])
+        if cube_quantity > minimum_cubes[cube_color]:
+            minimum_cubes[cube_color] = cube_quantity
+    return minimum_cubes
+
+def compute_game_power(game_minimum_cubes) -> int:
+    game_power = 1
+    for color in COLORS:
+        game_power *= game_minimum_cubes[color]
+    return game_power
+
+def main():
+    input = "input"
+    lines = read_file(input)
+    games_power_sum = 0
+    for line in lines:
+        print(line)
+        game_id, minimum_cubes = game_minimum_cubes(line)
+        game_power = compute_game_power(minimum_cubes)
+        print(f"\tMinimum cubes: {minimum_cubes}")
+        print(f"\tPower: {game_power}")
+
+        games_power_sum += game_power
+    print(f"Games power sum: {games_power_sum}")
+
+main()
