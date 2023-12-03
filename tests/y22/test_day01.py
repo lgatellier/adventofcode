72af57cb7d682eb3753e2ee022b18e72368cad8e
Léo GATELLIER <26511053+lgatellier@users.noreply.github.com>
2023-12-03 23:04:55 +0100

    test: add tests for Y22D01

--- tests/y22/test_day01.py
+++ tests/y22/test_day01.py
@@ -1,5 +1,20 @@
 from aoc.y22.day01 import part1 as puzzle
 
 
+def test_parse_next_elf():
+    lines = ["1", "2", "3", "4", "5", ""]
+    elf, pos = puzzle.parse_next_elf(lines, 0)
+    assert elf.total == 15
+    assert pos == 6
+
+
+def test_parse_elves():
+    lines = ["1", "2", "3", "4", "5", "", "6", "7", "8", "9", ""]
+    elves = puzzle.parse_elves(lines)
+    assert len(elves) == 2
+    assert elves[0].total == 15
+    assert elves[1].total == 30
+
+
 def test_main():
     assert puzzle.main("tests/y22/input_day01").total == 73211
